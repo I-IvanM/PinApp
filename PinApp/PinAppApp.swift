1015e9517557b3b89e29e5e5072eb1c50488348b
I-IvanM <i_mikhno@icloud.com>
2026-07-27 22:57:07 +0200

    Initial Commit

---
+++ PinApp/PinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinAppApp.swift
+//  PinApp
+//
+//  Created by I_IvanM on 27.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct PinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
